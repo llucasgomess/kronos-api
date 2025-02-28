403c7851442d2cc843fcf8349f71d44bd120b9b7
Lucas Gomes <llucas.gomes.dev@gmail.com>
2025-02-28 11:31:52 -0400

    :wrench: chore: add Zod setup

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,25 @@
-import fastifyCors from "@fastify/cors";
-import fastify, { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import fastifyCors from '@fastify/cors'
+import fastify, { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod'
 
 //Instaciar o servidor
-const server: FastifyInstance = fastify();
+const server: FastifyInstance = fastify().withTypeProvider<ZodTypeProvider>()
+
+//Configurações
+server.setSerializerCompiler(serializerCompiler)
+server.setValidatorCompiler(validatorCompiler)
 
 //Plugins
 server.register(fastifyCors)
 
 //rotas
-server.get("/", (req: FastifyRequest, replay: FastifyReply) => {
-  replay.status(200).send({ message: "servidor ok" });
-});
-
+server.get('/', (req: FastifyRequest, replay: FastifyReply) => {
+  replay.status(200).send({ message: 'servidor ok' })
+})
 
 //configurações de porta
 server.listen(
@@ -19,6 +27,6 @@ server.listen(
     port: 3000,
   },
   () => {
-    console.log("Server runnig port 3000");
+    console.log('Server runnig port 3000')
   }
-);
+)
