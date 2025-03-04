68c008843370b4e08d93566b747bb28e293e6526
Lucas Gomes <llucas.gomes.dev@gmail.com>
2025-03-04 12:35:52 -0400

    :tada: add swagger 'attorney'

--- src/constrollers/attorney.controller.ts
+++ src/constrollers/attorney.controller.ts
@@ -1,4 +1,5 @@
 import { FastifyInstance } from 'fastify'
+import z from 'zod'
 import { permission } from '../middlewares/permission.middleware'
 import {
   createAttorneyService,
@@ -11,27 +12,101 @@ import {
 export default async function attorneyController(server: FastifyInstance) {
   server.post(
     '/',
-    { preHandler: permission(['ADM', 'INSP']) },
+    {
+      preHandler: permission(['ADM', 'INSP']),
+      schema: {
+        summary: 'Rota para criar Advogado',
+        tags: ['Advogado'],
+        body: z.object({
+          nome: z.string().trim(),
+          oabNumero: z.string().trim(),
+        }),
+        // params: detentoParamsSchema,
+        response: {
+          201: z.object({
+            id: z.string().uuid(),
+            nome: z.string(),
+            oabNumero: z.string(),
+          }),
+          500: z.object({ error: z.string() }),
+        },
+      },
+    },
     createAttorneyService
   ),
     server.get(
       '/',
-      { preHandler: permission(['ADM', 'INSP']) },
+      {
+        preHandler: permission(['ADM', 'INSP']),
+        schema: {
+          summary: 'Rota para buscar todos os Advogado',
+          tags: ['Advogado'],
+        },
+      },
       getAllAttorneyService
     ),
     server.get(
       '/:id',
-      { preHandler: permission(['ADM', 'INSP']) },
+      {
+        preHandler: permission(['ADM', 'INSP']),
+        schema: {
+          summary: 'Rota para buscar Advogado por id',
+          tags: ['Advogado'],
+          params: z.object({
+            id: z.string().uuid(),
+          }),
+          response: {
+            201: z.object({
+              id: z.string().uuid(),
+              nome: z.string(),
+              oabNumero: z.string(),
+            }),
+            500: z.object({ error: z.string() }),
+          },
+        },
+      },
       getByIdAttorneyService
     ),
     server.delete(
       '/:id',
-      { preHandler: permission(['ADM', 'INSP']) },
+      {
+        preHandler: permission(['ADM', 'INSP']),
+        schema: {
+          summary: 'Rota para deletar o Advogado',
+          tags: ['Advogado'],
+          params: z.object({
+            id: z.string().uuid(),
+          }),
+          response: {
+            200: z.object({
+              message: z.string(),
+            }),
+            500: z.object({ error: z.string() }),
+          },
+        },
+      },
       deleteAttorneyService
     ),
     server.put(
       '/:id',
-      { preHandler: permission(['ADM', 'INSP']) },
+      {
+        preHandler: permission(['ADM', 'INSP']),
+        schema: {
+          summary: 'Rota para atualizar Advogados',
+          tags: ['Advogado'],
+          params: z.object({
+            id: z.string().uuid(),
+          }),
+          body: z.object({
+            nome: z.string().trim(),
+            oabNumero: z.string().trim(),
+          }),
+          response: {
+            200: z.object({ message: z.string() }),
+            500: z.object({ error: z.string() }),
+          },
+        },
+      },
       updateAttorneyService
     )
 }
