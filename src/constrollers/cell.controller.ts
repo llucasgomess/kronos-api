a2c53c41847176c9a47939f7ed9e7df22b0bba0a
Lucas Gomes <llucas.gomes.dev@gmail.com>
2025-03-04 12:25:07 -0400

    :tada: add swagger 'cell'

--- src/constrollers/cell.controller.ts
+++ src/constrollers/cell.controller.ts
@@ -1,7 +1,39 @@
 import { FastifyInstance } from 'fastify'
+import z from 'zod'
 import { permission } from '../middlewares/permission.middleware'
 import { createCellService } from '../services/cell.service'
 
 export default async function cellController(server: FastifyInstance) {
-  server.post('/', { preHandler: permission(['ADM']) }, createCellService)
+  server.post(
+    '/',
+    {
+      preHandler: permission(['ADM']),
+      schema: {
+        summary: 'Rota para criar cela',
+        tags: ['Celas'],
+        body: z.object({
+          numero: z.number(),
+          capacidade: z.number(),
+          pavilhao: z.string(),
+        }),
+        response: {
+          200: z.object({
+            id: z.string().uuid(),
+            celaId: z.string().uuid(),
+            detentoId: z.string().uuid(),
+            dataAlocacao: z.date(),
+          }),
+          201: z.object({
+            id: z.string().uuid(),
+            numero: z.number(),
+            capacidade: z.number(),
+            pavilhao: z.string(),
+          }),
+          400: z.object({ error: z.string() }),
+          500: z.object({ error: z.string() }),
+        },
+      },
+    },
+    createCellService
+  )
 }
